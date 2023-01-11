626d6ae4fcd6225c56da9cc29b2145a73eef827d
mikupeto <mikupeto@proton.me>
2023-01-11 08:35:47 +0800

    Fix bugs

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,6 +10,7 @@ sed -i "s#VMESS_WSPATH#${VMESS_WSPATH}#g;s#VLESS_WSPATH#${VLESS_WSPATH}#g;s#TROJ
 sed -i "s#RELEASE_RANDOMNESS#${RELEASE_RANDOMNESS}#g" /etc/supervisor/conf.d/supervisord.conf
 
 # 伪装 xray 执行文件
+RELEASE_RANDOMNESS=$(tr -dc 'A-Za-z0-9' </dev/urandom | head -c 6)
 mv xray ${RELEASE_RANDOMNESS}
 cat config.json | base64 > config
 rm -f config.json
@@ -18,4 +19,5 @@ rm -f config.json
 [ -n "${NEZHA_SERVER}" ] && [ -n "${NEZHA_PORT}" ] && [ -n "${NEZHA_KEY}" ] && wget https://raw.githubusercontent.com/naiba/nezha/master/script/install.sh -O nezha.sh && chmod +x nezha.sh && ./nezha.sh install_agent ${NEZHA_SERVER} ${NEZHA_PORT} ${NEZHA_KEY}
 
 nginx
-base64 -d config > config.json; ./${RELEASE_RANDOMNESS} -config=config.json
+base64 -d config > config.json
+./${RELEASE_RANDOMNESS} -config=config.json
